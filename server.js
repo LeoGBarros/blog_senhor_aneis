2fccb7a8f244a504e67a5f3dbd6d590075421a0e
Leonardo Barros <leoguedesbarros@gmail.com>
2024-10-09 23:39:52 -0300

    ajuste formatação

--- server.js
+++ server.js
@@ -12,27 +12,22 @@ const apiToken = 'XkBH9xRqvXkg7bFhHTWf';
 app.get('/character/:race', async (req, res) => {
   const race = req.params.race;
   try {
-      // Faz a requisição para a API
       const response = await axios.get('https://the-one-api.dev/v2/character', {
           headers: {
-              Authorization: `Bearer ${apiToken}` // Inclui o token de autorização
+              Authorization: `Bearer ${apiToken}` 
           },
           params: {
-              race: race // Envia o parâmetro de raça
+              race: race 
           }
       });
 
-      // Verifica se há personagens no retorno
       if (response.data && response.data.docs) {
-          // Retorna apenas os primeiros 10 personagens
           const characters = response.data.docs.slice(0, 10);
-          res.json(characters); // Retorna os personagens como JSON
+          res.json(characters); 
       } else {
-          // Caso não haja personagens
           res.status(404).json({ error: 'Nenhum personagem encontrado para esta raça' });
       }
   } catch (error) {
-      // Tratamento de erro
       console.error('Erro ao buscar personagens:', error);
       res.status(500).json({ error: 'Erro ao buscar personagens da API' });
   }
